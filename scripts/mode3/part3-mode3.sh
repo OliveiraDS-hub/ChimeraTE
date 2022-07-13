c3d7529448fa9867aa02c00d3e9967169c17d053
Daniel Oliveira <daniel.sdo2015@gmail.com>
2022-07-13 15:05:11 +0200

    Delete scripts/mode3 directory

--- scripts/mode3/part3-mode3.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir  "$OUTPUT"/"$sample"/expression 2>/dev/null
-align_and_estimate_abundance.pl --transcripts "$OUTPUT"/"$sample"/Trinity.fasta --seqType fq --left "$mate1_rep" --right "$mate2_rep" --est_method RSEM --aln_method bowtie2 --SS_lib_type "$STRANDED" --thread_count "$THREADS" --gene_trans_map  "$OUTPUT"/"$sample"/Trinity.fasta.gene_trans_map --output_dir "$OUTPUT"/"$sample"/expression --prep_reference
